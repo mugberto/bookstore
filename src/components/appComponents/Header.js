bc11ab881cd86760ed8f386ad7e6581ac6fc9a7d
Hubert MUGABO <mugberto@gmail.com>
2021-08-24 21:33:39 +0200

    Add user icon

--- src/components/appComponents/Header.js
+++ src/components/appComponents/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 function Header() {
@@ -9,7 +11,7 @@ function Header() {
         <Link to="/" className="brand-name">Boostore CMS</Link>
         <Link to="/" className="nav-link">BOOKS</Link>
         <Link to="/categories" className="nav-link">CATEGORIES</Link>
-        <span className="user-avator"><i className="fa fa-user"></i></span>
+        <span className="user-avator"><FontAwesomeIcon icon={faUser} /></span>
       </nav>
     </header>
   );
