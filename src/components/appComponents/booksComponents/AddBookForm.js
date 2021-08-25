a81e29628bdd5983fe52a7fda69f149c6e180866
Hubert MUGABO <mugberto@gmail.com>
2021-08-25 05:21:41 +0200

    Add form handler function

--- src/components/appComponents/booksComponents/AddBookForm.js
+++ src/components/appComponents/booksComponents/AddBookForm.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddBookForm() {
+  const [formState, setFormState] = useState({
+    title: 'Book title',
+    category: 'Category',
+  });
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit() {
+
+  }
+
   return (
     <div>
       <h2>Add NEW BOOK</h2>
-      <form>
-        <input type="text" name="title" placeholder="Book title" />
-        <select name="category" defaultValue="Category">
-          <option value="">Category</option>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="Book title" value={formState.title} onChange={handleChange} />
+        <select name="category" value={formState.category} onChange={handleChange}>
+          <option value="" hidden>Category</option>
           <option value="Sci-Fi">Sci-Fi</option>
           <option value="Action">Action</option>
         </select>
-        <button type="button" className="add-btn">ADD BOOK</button>
+        <button type="submit" className="add-btn">ADD BOOK</button>
       </form>
     </div>
   );
