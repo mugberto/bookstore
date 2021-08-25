aadc8ca2b083d8148e69ea286bd4759b541bbdbe
Hubert MUGABO <mugberto@gmail.com>
2021-08-25 04:21:24 +0200

    Edit books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -25,6 +25,33 @@ const defaultState = [
   },
 ];
 
-export default function books(state = defaultState) {
-  return state;
+export default function books(state = defaultState, action) {
+  switch(action.type) {
+    case ADD_BOOK:
+      return state.concat({
+        id: Date.now(),
+        title: action.payload.title,
+        author: action.payload.author,
+        category: action.payload.category,
+        progress: 0,
+      });
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id );
+    default:
+      return state;
+  }
+}
+
+export function addBook(payload) {
+  return {
+    type: ADD_BOOK,
+    payload,
+  };
+}
+
+export function removeBook(payload) {
+  return {
+    type: REMOVE_BOOK,
+    payload,
+  }
 }
