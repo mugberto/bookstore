652a3be574d4175fc0d5545f9c32107a2161712f
Hubert MUGABO <mugberto@gmail.com>
2021-08-26 01:16:16 +0200

    Refactor for adding a book async to API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const APP_ID = 'abMkyj5tLtp4TVRuI7vE';
 
 const defaultState = [];
 
@@ -7,11 +8,11 @@ export default function books(state = defaultState, action) {
   switch (action.type) {
     case ADD_BOOK:
       return state.concat({
-        id: Date.now(),
+        id: action.payload.item_id,
         title: action.payload.title,
-        author: action.payload.author,
+        author: 'Suzanne Collins',
         category: action.payload.category,
-        progress: 0,
+        progress: 74,
       });
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
@@ -31,5 +32,18 @@ export function removeBook(payload) {
   return {
     type: REMOVE_BOOK,
     payload,
+  };
+}
+
+export function addBookAPI(payload) {
+  const bookDetails = { ...payload, item_id: Date.now() };
+  return async function addBookThunk(dispatch) {
+    fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}/books`, {
+      method: 'POST',
+      body: JSON.stringify(bookDetails),
+      headers: {
+        'content-type': 'application/json',
+      },
+    }).then((response) => response.status === 201 && dispatch(addBook(bookDetails)));
   };
 }
