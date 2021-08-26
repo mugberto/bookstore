149d9a4c1af35f277f1fa331f363405adaef2b4c
Hubert MUGABO <mugberto@gmail.com>
2021-08-26 23:54:18 +0200

    Fix linter errors

--- src/components/appComponents/Header.js
+++ src/components/appComponents/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
@@ -8,9 +8,9 @@ function Header() {
   return (
     <header className="app-header">
       <nav className="app-nav">
-        <Link to="/" className="brand">Boostore CMS</Link>
-        <Link to="/" className="nav-link">BOOKS</Link>
-        <Link to="/categories" className="nav-link">CATEGORIES</Link>
+        <NavLink to="/" className="brand">Boostore CMS</NavLink>
+        <NavLink to="/" className="nav-link">BOOKS</NavLink>
+        <NavLink to="/categories" className="nav-link">CATEGORIES</NavLink>
         <span className="user-avator"><FontAwesomeIcon icon={faUser} /></span>
       </nav>
     </header>
